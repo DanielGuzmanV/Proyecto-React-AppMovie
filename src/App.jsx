5e14e2e837d0e605e1c02b3c451b544db074a50a
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-22 12:06:31 -0400

    Inicio del proyecto -App Movies-

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <header>
+      <h1>App - Peliculas</h1>
+    </header>
+  )
+}
+
+export default App
