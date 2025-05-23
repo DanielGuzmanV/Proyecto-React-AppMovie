347fa02e860fbeddc62de694c31053090d4034ba
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-23 09:50:03 -0400

    se agrego la api de themoviedb

---
+++ src/data/httpClient.js
@@ -0,0 +1,27 @@
+
+const API = "https://api.themoviedb.org/";
+
+export async function get1(path) {
+  try {
+    const response = await fetch(API + path, {
+      headers: {
+        Authorization: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTk2NzQ2NTY3YjkyMjQ5MDNmMDYzNDA5MDYwNDA1MyIsIm5iZiI6MTcyODY1MzA3OC42Nywic3ViIjoiNjcwOTI3MTY2YWRmNjYyMDIxMzAzN2JiIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.6XIqXySr_9D-3e7iN-DQL5nJz0wX9hmhI2VnJx7qkj0",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+
+    if(!response.ok) {
+      throw new Error('Error en la peticion');
+    }
+    const data = await response.json();
+    return data;
+    
+  } catch (error) {
+    console.error("Error al obtener datos:", error)
+  }
+}
+
+
+
+
+
